efdb1c600d9df845ec0b9fb03f23a80a8203b360
Varun singhal <78909090+varun050303@users.noreply.github.com>
2024-11-17 23:56:56 +0530

    Update gptClient.js

--- gptClient.js
+++ gptClient.js
@@ -1,8 +1,7 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-    // apiKey: process.env.OPENAI_API_KEY,
-    apiKey: "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"
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 export default openai
